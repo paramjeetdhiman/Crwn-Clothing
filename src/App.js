f28b9583c2d8cfce805f3a7013adab0f38f90de0
Paramjeet Dhiman <paramjeet@montaigne.co>
2020-02-09 13:13:07 +0530

    ADD: Header Component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage-component';
 import { ReactRouterDomExample } from './ReactRouterDomExample';
+import Header from './components/header/header-component';
+
 import ShopPage from './pages/shop/shop-component';
 
 const App = () => {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />

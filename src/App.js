330ad3e9d17ebabe0f6cec94838096fe165b912f
Paramjeet Dhiman <paramjeet@montaigne.co>
2020-02-09 12:55:07 +0530

    ADD: Components ShopPage, CollectionPreview, CollectionItem

--- src/App.js
+++ src/App.js
@@ -4,19 +4,14 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage-component';
 import { ReactRouterDomExample } from './ReactRouterDomExample';
-
-const HatsPage = () => (
-  <>
-    <h1>HATS PAGE...</h1>
-  </>
-);
+import ShopPage from './pages/shop/shop-component';
 
 const App = () => {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
